c115b5ac8f53b283600898179afff70d1449ca18
RizalPurwantoBalen <rizal@balen.id>
2023-08-24 23:59:51 +0700

    activity list with icons added

---
+++ src/components/ActivityBullet.js
@@ -0,0 +1,21 @@
+
+export default function ActivityBullet () {
+    return (
+        <svg
+        width="21"
+        height="106"
+        viewBox="0 0 21 106"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          cx="10.5"
+          cy="10.5"
+          r="9.5"
+          stroke="#FF814A"
+          stroke-width="2"
+        />
+        <line cx="2" x1="10" x2="10" y1="18" y2="110" stroke="#5C6CA5" />
+      </svg>
+    )
+}
--- src/components/ActivityOverview.js
+++ src/components/ActivityOverview.js
@@ -1,25 +1,31 @@
 import { styled } from "styled-components";
 import { AiOutlineArrowUp } from "react-icons/ai";
 import { IconContext } from "react-icons/lib";
+import ActivityBullet from "./ActivityBullet";
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: flex-start;
-  width: 430px;
+  width: 390px;
   height: 722px;
   background-color: #1c243f;
   margin-top: 20px;
-  margin-left:20px;
+  margin-left: 20px;
   border-radius: 20px;
-  margin-bottom:20px;
+  margin-bottom: 20px;
+  gap: 10px;
+  padding: 20px;
 `;
 
 const ActivityListContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: flex-start;
+  width: 100%;
+  margin-top:10px;
+
 `;
 
 const ActivityTitle = styled.div`
@@ -36,6 +42,57 @@ const ActivityStatus = styled.div`
   flex-direction: row;
   justify-content: center;
 `;
+
+const ActivityItem = styled.div`
+  width: 80%;
+  height: 16%;
+  display: flex;
+`;
+
+const ActivityInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 120%;
+  margin-top: 3px;
+  font-size: 14px;
+  font-weight: 400;
+  color: #ffffff;
+  align-items: flex-start;
+  padding-left: 8px;
+`;
+
+const ActivityDate = styled.div`
+  font-size: 12px;
+  font-weight: 400;
+  color: #9f9f9f;
+`;
+
+const ActivityText = styled.div`
+display: flex;
+`;
+
+const activityArr = [
+  {
+    text: <ActivityText><strong>Nick Mark</strong> Mentioned <strong>Sara Smith</strong> In New Post</ActivityText>,
+    date: "11 JUL 8:10 PM",
+  },
+  {
+    text: <ActivityText><strong>The Post Name</strong> was removed by <strong>Nick Mark</strong></ActivityText>,
+    date: "11 JUL 9:10 PM",
+  },
+  {
+    text: <ActivityText><strong>Patrick Sulivan</strong> Published a New Post</ActivityText>,
+    date: "12 JUL 10:10 PM",
+  },
+  {
+    text: <ActivityText><strong>240+ users</strong> have subscribed to Newsletter #1</ActivityText>,
+    date: "18 JUL 19:10 PM",
+  },
+  {
+    text: <ActivityText><strong>The Post Name</strong> was suspended by <strong>Nick Mark</strong></ActivityText>,
+    date: "11 JUL 9:10 PM",
+  },
+];
 
 export default function ActivityOverview() {
   return (
@@ -57,7 +114,18 @@ export default function ActivityOverview() {
         <div>16% this month</div>
       </ActivityStatus>
 
-      <ActivityListContainer></ActivityListContainer>
+      <ActivityListContainer>
+        {activityArr.map((el) => (
+              <ActivityItem>
+              <ActivityBullet></ActivityBullet>
+              <ActivityInfo>
+              {el.text}
+                <ActivityDate>{el.date}</ActivityDate>
+              </ActivityInfo>
+            </ActivityItem>
+        ))}
+       
+      </ActivityListContainer>
     </Container>
   );
 }
